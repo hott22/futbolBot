a69755af8966b1b942afd692c33fa8cc0ceb3618
Bobby <mif210210@gmail.com>
2022-06-13 01:44:48 +0300

    Добавил отправку по времени

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ filter_text = ['+', '-', '+1', '-1']
 app.add_handler(CommandHandler("help", help_command))
 
 app.add_handler(MessageHandler(filters.Text(filter_text), callback=run))
+app.add_handler(CommandHandler("del", del_command))
 app.add_handler(MessageHandler(filters.TEXT, callback=log))
+app.job_queue.run_repeating(tela_tela, interval=3600, first=1)
 
 print('start')
 app.run_polling(stop_signals=None)
